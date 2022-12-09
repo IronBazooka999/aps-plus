806fd0e3dcff5ef1e3f5653a90d3caa8f9552187
Damocles <lunahackley@gmail.com>
2022-12-09 14:08:45 -0600

    Adjust syntax and improve exports.chooseChance()
    - Removed all semicolons (they are NOT necessary in JavaScript, you should not be forced to remember to use them constantly with this language if you don't intend on working with higher up statically typed languages like C, C++, Rust, etc.
    - Removed unnecessary bracketing of arrow functions, while loops, for loops, and if structures that were one line long.
    - Changed exports.chooseChance to not use a forEach() method and instead a for of loop since they are significantly more efficient.

--- server/lib/random.js
+++ server/lib/random.js
@@ -1,98 +1,83 @@
 /*jslint node: true */
-"use strict";
+"use strict"
 
 // Seed math
 
-exports.random = (x) => {
-  return x * Math.random();
-};
+exports.random = x => x * Math.random()
 
-exports.randomAngle = () => {
-  return Math.PI * 2 * Math.random();
-};
+exports.randomAngle = () => Math.PI * 2 * Math.random()
 
-exports.randomRange = (min, max) => {
-  return Math.random() * (max - min) + min;
-};
+exports.randomRange = (min, max) => Math.random() * (max - min) + min
 
-exports.irandom = (i) => {
-  let max = Math.floor(i);
-  return Math.floor(Math.random() * (max + 1)); //Inclusive
-};
+exports.irandom = i => {
+  let max = Math.floor(i)
+  return Math.floor(Math.random() * (max + 1)) //Inclusive
+}
 
 exports.irandomRange = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Inclusive
-};
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min //Inclusive
+}
 
 exports.gauss = (mean, deviation) => {
-  let x1, x2, w;
+  let x1, x2, w
   do {
-    x1 = 2 * Math.random() - 1;
-    x2 = 2 * Math.random() - 1;
-    w = x1 * x1 + x2 * x2;
-  } while (0 == w || w >= 1);
+    x1 = 2 * Math.random() - 1
+    x2 = 2 * Math.random() - 1
+    w = x1 * x1 + x2 * x2
+  } while (0 == w || w >= 1)
 
-  w = Math.sqrt((-2 * Math.log(w)) / w);
-  return mean + deviation * x1 * w;
-};
+  w = Math.sqrt((-2 * Math.log(w)) / w)
+  return mean + deviation * x1 * w
+}
 
 exports.gaussInverse = (min, max, clustering) => {
-  let range = max - min;
-  let output = exports.gauss(0, range / clustering);
+  let range = max - min
+  let output = exports.gauss(0, range / clustering)
 
-  while (output < 0) {
-    output += range;
-  }
+  while (output < 0)
+    output += range
 
-  while (output > range) {
-    output -= range;
-  }
+  while (output > range)
+    output -= range
 
-  return output + min;
-};
+  return output + min
+}
 
 exports.gaussRing = (radius, clustering) => {
-  let r = exports.random(Math.PI * 2);
-  let d = exports.gauss(radius, radius * clustering);
+  let r = exports.random(Math.PI * 2)
+  let d = exports.gauss(radius, radius * clustering)
   return {
     x: d * Math.cos(r),
     y: d * Math.sin(r),
-  };
-};
+  }
+}
 
-exports.chance = (prob) => {
-  return exports.random(1) < prob;
-};
+exports.chance = prob => exports.random(1) < prob
 
-exports.dice = (sides) => {
-  return exports.random(sides) < 1;
-};
+exports.dice = sides => exports.random(sides) < 1
 
-exports.choose = (arr) => {
-  return arr[exports.irandom(arr.length - 1)];
-};
+exports.choose = (arr) => arr[exports.irandom(arr.length - 1)]
 
 exports.chooseN = (arr, n) => {
-  let o = [];
-  for (let i = 0; i < n; i++) {
-    o.push(arr.splice(exports.irandom(arr.length - 1), 1)[0]);
-  }
-  return o;
-};
+  let o = []
+  for (let i = 0; i < n; i++)
+    o.push(arr.splice(exports.irandom(arr.length - 1), 1)[0])
+  return o
+}
 
 exports.chooseChance = (...arg) => {
-  let totalProb = 0;
-  arg.forEach(function (value) {
-    totalProb += value;
-  });
-  let answer = exports.random(totalProb);
+  let totalProb = 0
+  for (let value of arg)
+    totalProb += value
+
+  let answer = exports.random(totalProb)
   for (let i = 0; i < arg.length; i++) {
-    if (answer < arg[i]) return i;
-    answer -= arg[i];
+    if (answer < arg[i]) return i
+    answer -= arg[i]
   }
-};
+}
 
 exports.chooseBotName = () => {
   return exports.choose([
@@ -120,8 +105,8 @@ exports.chooseBotName = () => {
     "Xerxes",
     "Yvonne",
     "Zachary",
-  ]);
-};
+  ])
+}
 
 exports.chooseBossName = (code, n) => {
   switch (code) {
@@ -140,7 +125,7 @@ exports.chooseBossName = (code, n) => {
           "Apollon",
         ],
         n
-      );
+      )
     case "castle":
       return exports.chooseN(
         [
@@ -159,27 +144,27 @@ exports.chooseBossName = (code, n) => {
           "Bilhorod",
         ],
         n
-      );
+      )
     case "zaphkiel":
-      return "Zaphkiel";
+      return "Zaphkiel"
     case "paladin":
-      return "Paladin";
+      return "Paladin"
     case "theia":
-      return "Theia";
+      return "Theia"
     case "freyja":
-      return "Freyja";
+      return "Freyja"
     case "nyx":
-      return "Nyx";
+      return "Nyx"
     case "alviss":
-      return "Alviss";
+      return "Alviss"
     case "tyr":
-      return "Tyr";
+      return "Tyr"
     case "fiolnir":
-      return "Fiolnir";
+      return "Fiolnir"
     case "ragnarok":
-      return "Ragnarok";
+      return "Ragnarok"
     case "kronos":
-      return "Kronos";
+      return "Kronos"
     case "legion":
       return exports.chooseN(
         [
@@ -191,16 +176,16 @@ exports.chooseBossName = (code, n) => {
           "Ceres",
         ],
         n
-      );
+      )
     case "pumpkin":
-      return "Jack Skeleton";
+      return "Jack Skeleton"
     case "santa":
-      return "Santa";
+      return "Santa"
     case "krampus":
-      return "Krampus";
+      return "Krampus"
     case "vesta":
-      return "Vesta";
+      return "Vesta"
     default:
-      return "missingno";
+      return "missingno"
   }
-};
+}
