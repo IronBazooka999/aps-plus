33c154c38c7239f073bba26171670ede6939acae
TGS <93544652+TGS5994@users.noreply.github.com>
2023-01-28 09:52:05 +0000

    zoom fix

--- public/app.js
+++ public/app.js
@@ -121,22 +121,6 @@ import * as socketStuff from "./lib/socketInit.js";
     );
   }
   function getColor(colorNumber) {
-    // 0-24, 30-37 and 39-41 are kept the same as standard Arras for consistency.
-    // For Woomy-Arras color IDs between 20 and 44, increment the number by 30 so they'll work as intended.
-
-    // The following Woomy-Arras color IDs are not included as they are duplicates of another color.
-    /*
-    Original Color ID -> Replacement Color ID
-    68 -> 108
-    226 -> 13
-    */
-    // Other Woomy-Arras color IDs will work fine with no changes.
-
-    // The following Raxor.io color IDs are not included as they are duplicates of another color.
-    /*
-    Original Color ID -> Replacement Color ID
-    43 -> 190
-    */
     switch (colorNumber) {
       case 0:
         return color.teal;
@@ -189,10 +173,6 @@ import * as socketStuff from "./lib/socketInit.js";
         return 150 > Date.now() % 300 ? color.red : color.grey;
       case 24:
         return 150 > Date.now() % 300 ? color.grey : color.red;
-      case 28:
-        return "#55CDFC";
-      case 29:
-        return "#F7A8B8";
       case 30:
         return "#a913cf";
       case 31:
@@ -221,340 +201,12 @@ import * as socketStuff from "./lib/socketInit.js";
           2e3 > Date.now() % 4e3 ? "#55cdfc" : "#f7a8b8",
           5 * Math.sin(((Date.now() % 2e3) / 2e3) * Math.PI) - 2
         );
-      case 38:
-        return getRainbow(
-          "#ce0063 #9c319c #00319c".split(" ")[
-            Math.floor((Date.now() / 200) % 3)
-          ],
-          "#ce0063 #9c319c #00319c".split(" ")[
-            Math.floor((Date.now() / 200) % 3)
-          ],
-          (Date.now() / 200) % 1
-        );
       case 39:
         return "#654321";
       case 40:
         return "#e58100";
       case 41:
         return "#267524";
-      case 42:
-        return "#123456";
-      case 50:
-        return "#307A76";
-      case 51:
-        return "#47F51E";
-      case 52:
-        return "#9264EF";
-      case 53:
-        return "#1D00FF";
-      case 54:
-        return "#B35ED8";
-      case 55:
-        return "#0531CB";
-      case 56:
-        return "#FDA54D";
-      case 57:
-        return "#3761D1";
-      case 58:
-        return "#AB1515";
-      case 59:
-        return "#44AA34";
-      case 60:
-        return "#EEF5A7";
-      case 61:
-        return "#8BFE6A";
-      case 62:
-        return "#FAC577";
-      case 63:
-        return "#8AFF8A";
-      case 64:
-        return "#666666";
-      case 65:
-        return "#F37C20";
-      case 66:
-        return "#E85DDF";
-      case 67:
-        return "#FFFF00";
-      case 69:
-        return "#FFBF00";
-      case 70:
-        return "#57C8C2";
-      case 71:
-        return "#A6E1DE";
-      case 72:
-        return "#BF0731";
-      case 73:
-        return "#F80A41";
-      case 74:
-        return "#00EEA4";
-      case 100:
-        return "#FF0000";
-      case 101:
-        return "#FF1A00";
-      case 102:
-        return "#FF2A00";
-      case 103:
-        return "#FF4300";
-      case 104:
-        return "#FF5D00";
-      case 105:
-        return "#FF7200";
-      case 106:
-        return "#FF7700";
-      case 107:
-        return "#FF9400";
-      case 108:
-        return "#FF9900";
-      case 109:
-        return "#FFA500";
-      case 110:
-        return "#FFBB00";
-      case 111:
-        return "#FFCC00";
-      case 112:
-        return "#FFDD00";
-      case 113:
-        return "#FFE900";
-      case 114:
-        return "#FFFA00";
-      case 115:
-        return "#EEFF00";
-      case 116:
-        return "#DDFF00";
-      case 117:
-        return "#D0FF00";
-      case 118:
-        return "#B6FF00";
-      case 119:
-        return "#AAFF00";
-      case 120:
-        return "#88FF00";
-      case 121:
-        return "#6EFF00";
-      case 122:
-        return "#54FF00";
-      case 123:
-        return "#32FF00";
-      case 124:
-        return "#19FF00";
-      case 125:
-        return "#04FF00";
-      case 126:
-        return "#00FF15";
-      case 127:
-        return "#00FF26";
-      case 128:
-        return "#00FF3F";
-      case 129:
-        return "#00FF55";
-      case 130:
-        return "#00FF6E";
-      case 131:
-        return "#00FF7F";
-      case 132:
-        return "#00FF99";
-      case 133:
-        return "#00FFA5";
-      case 134:
-        return "#00FFBB";
-      case 135:
-        return "#00FFCB";
-      case 136:
-        return "#00FFD8";
-      case 137:
-        return "#00FFED";
-      case 138:
-        return "#00FFFA";
-      case 139:
-        return "#00E9FF";
-      case 140:
-        return "#00D8FF";
-      case 141:
-        return "#00C3FF";
-      case 142:
-        return "#00BBFF";
-      case 143:
-        return "#00AEFF";
-      case 144:
-        return "#00A1FF";
-      case 145:
-        return "#0090FF";
-      case 146:
-        return "#007FFF";
-      case 147:
-        return "#0077FF";
-      case 148:
-        return "#006EFF";
-      case 149:
-        return "#005DFF";
-      case 150:
-        return "#0048FF";
-      case 151:
-        return "#0037FF";
-      case 152:
-        return "#0026FF";
-      case 153:
-        return "#0019FF";
-      case 154:
-        return "#0004FF";
-      case 155:
-        return "#0C00FF";
-      case 156:
-        return "#2200FF";
-      case 157:
-        return "#2E00FF";
-      case 158:
-        return "#3B00FF";
-      case 159:
-        return "#5400FF";
-      case 160:
-        return "#6A00FF";
-      case 161:
-        return "#7F00FF";
-      case 162:
-        return "#9000FF";
-      case 163:
-        return "#A100FF";
-      case 164:
-        return "#B600FF";
-      case 165:
-        return "#BF00FF";
-      case 166:
-        return "#D000FF";
-      case 167:
-        return "#DC00FF";
-      case 168:
-        return "#E900FF";
-      case 169:
-        return "#FA00FF";
-      case 170:
-        return "#FF00F6";
-      case 171:
-        return "#FF00E1";
-      case 172:
-        return "#FF00CB";
-      case 173:
-        return "#FF00B6";
-      case 174:
-        return "#FF00AA";
-      case 175:
-        return "#FF00A5";
-      case 176:
-        return "#FF0090";
-      case 177:
-        return "#FF007B";
-      case 178:
-        return "#FF006E";
-      case 179:
-        return "#FF005D";
-      case 180:
-        return "#FF0059";
-      case 181:
-        return "#FF0043";
-      case 182:
-        return "#FF003B";
-      case 183:
-        return "#FF0026";
-      case 184:
-        return "#FF001D";
-      case 185:
-        return "#FF000C";
-      case 186:
-        return "#AA8A8B";
-      case 187:
-        return "#BC7B7D";
-      case 188:
-        return "#CD6D70";
-      case 189:
-        return "#DF5E62";
-      case 190:
-        return "#CB6F3C";
-      case 191:
-        return "#00D2FF";
-      case 192:
-        return "#003399";
-      case 193:
-        return "#BDBDBD";
-      case 194:
-        return "#B7410E";
-      case 195:
-        return "#65F0EC";
-      case 196:
-        return "#EAB57A";
-      case 197:
-        return "#E6E600";
-      case 198:
-        return "#E69138";
-      case 199:
-        return "#EA9999";
-      case 200:
-        return "#CCFF00";
-      case 201:
-        return "#800000";
-      case 202:
-        return "#F7EB73";
-      case 203:
-        return "#9A5BAB";
-      case 204:
-        return "#ED7332";
-      case 205:
-        return "#FDA2A2";
-      case 206:
-        return "#00428B";
-      case 207:
-        return "#FF8000";
-      case 208:
-        return "#FFB66C";
-      case 209:
-        return "#C0C0C0";
-      case 210:
-        return "#FFFF80";
-      case 211:
-        return "#9B59D0";
-      case 212:
-        return "#996B6D";
-      case 213:
-        return "#FE9774";
-      case 214:
-        return "#77E2FB";
-      case 215:
-        return "#EFA900";
-      case 216:
-        return "#FC8208";
-      case 217:
-        return "#6CF1EE";
-      case 218:
-        return "#FFD900";
-      case 219:
-        return "#FFAE40";
-      case 220:
-        return "#FFA600";
-      case 221:
-        return "#FF0080";
-      case 222:
-        return "#00FFFF";
-      case 223:
-        return "#00BFFF";
-      case 224:
-        return "#99D9EA";
-      case 225:
-        return "#6DB5C9";
-      case 227:
-        return "#D5095B";
-      case 228:
-        return "#FF7F00";
-      case 229:
-        return "#A277FB";
-      case 230:
-        return "#BA8939";
-      case 231:
-        return "#5AE3E3";
-      case 232:
-        return "#FF6600";
-      case 233:
-        return "#FF9955";
-      case 234:
-        return "#D4AF37";
       default:
         return "#00000000";
     }
@@ -736,6 +388,7 @@ import * as socketStuff from "./lib/socketInit.js";
     util.retrieveFromLocalStorage("optPredictive");
     util.retrieveFromLocalStorage("optFancy");
     util.retrieveFromLocalStorage("coloredHealthbars");
+    util.retrieveFromLocalStorage("centerTank");
     util.retrieveFromLocalStorage("optColors");
     util.retrieveFromLocalStorage("optNoPointy");
     util.retrieveFromLocalStorage("optBorders");
@@ -827,6 +480,8 @@ import * as socketStuff from "./lib/socketInit.js";
     util.submitToLocalStorage("coloredHealthbars");
     config.graphical.coloredHealthbars =
       document.getElementById("coloredHealthbars").checked;
+    util.submitToLocalStorage("centerTank");
+    config.graphical.centerTank = document.getElementById("centerTank").checked;
     util.submitToLocalStorage("optPredictive");
     config.lag.unresponsive = document.getElementById("optPredictive").checked;
     util.submitToLocalStorage("optBorders");
@@ -1694,7 +1349,8 @@ import * as socketStuff from "./lib/socketInit.js";
         TextObj(),
         TextObj(),
       ],
-      upgradeNames: [ // This sets the limit for the number of tanks, along with upgradeKeys below. The default limit is 30.
+      upgradeNames: [
+        // This sets the limit for the number of tanks, along with upgradeKeys below. The default limit is 30.
         TextObj(),
         TextObj(),
         TextObj(),
@@ -1726,7 +1382,8 @@ import * as socketStuff from "./lib/socketInit.js";
         TextObj(),
         TextObj(),
       ],
-      upgradeKeys: [ // This sets the limit for the number of tanks, along with upgradeNames above. The default limit is 30.
+      upgradeKeys: [
+        // This sets the limit for the number of tanks, along with upgradeNames above. The default limit is 30.
         TextObj(),
         TextObj(),
         TextObj(),
@@ -1895,9 +1552,13 @@ import * as socketStuff from "./lib/socketInit.js";
               ? Math.atan2(global.target.y, global.target.x)
               : util.lerpAngle(instance.render.f, instance.facing, 0.15, true);
           let x =
-              instance.id === gui.playerid ? 0 : ratio * instance.render.x - px,
+              instance.id === gui.playerid && config.graphical.centerTank
+                ? 0
+                : ratio * instance.render.x - px,
             y =
-              instance.id === gui.playerid ? 0 : ratio * instance.render.y - py;
+              instance.id === gui.playerid && config.graphical.centerTank
+                ? 0
+                : ratio * instance.render.y - py;
           x += global.screenWidth / 2;
           y += global.screenHeight / 2;
           drawEntity(
