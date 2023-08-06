1522ee978f10ec29989b3dceb09fa6698cf20e99
Taureon <45183108+Taureon@users.noreply.github.com>
2023-08-07 00:15:48 +0200

    prep for gamemode config separation
    hey i heard you like files so i made like a dozen empty ones just for you!

---
+++ server/modules/setup/gamemodeconfigs/growth.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SKILL_CAP: 1024,
+    LEVEL_SKILL_POINT_FUNCTION: level => {
+        if (level < 2) return 0;
+        if (level <= 40) return 1;
+        if (level <= 45 && level & 1 == 1) return 1;
+        if (level % 3 == 1) return 1;
+    },
+};
