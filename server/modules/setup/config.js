6e286f1b5362dc68bf2e96581ed5814e1e5458f5
CLASHTEST <132932305+CLASHTEST@users.noreply.github.com>
2023-08-11 16:56:06 +0200

    update

--- server/modules/setup/config.js
+++ server/modules/setup/config.js
@@ -1,6 +1,6 @@
 let output = require("../../config.js");
 
-const gamemodes = ["opentdm", "space"];
+const gamemodes = ["tdm", "domination"];
 
 for (let gamemode of gamemodes) {
     let mode = require(`./gamemodeconfigs/${gamemode}.js`);
@@ -18,7 +18,7 @@ for (let gamemode of gamemodes) {
         }
     }
 }
-output.BOTS = 9;
+
 /*output.gameModeName = gamemode;
 if (["Tag", "Domination", "Mothership"].includes(gamemode)) {
     output.gameModeName = `${output.TEAMS} TDM ${gamemode}`;
