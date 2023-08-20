2d70bf4981933104c41826dd7efd356c4c84c8b4
TGS <93544652+TGS5994@users.noreply.github.com>
2023-08-20 17:01:23 +0000

    Update dev.js

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -717,19 +717,12 @@ exports.developer.UPGRADES_TIER_0 = ["healer", "basic", "lancer", "gameAdminMenu
     exports.bossesMenu.UPGRADES_TIER_0 = ["sentries", "celestialBosses", "eliteBosses", "strangeBosses", "ironclad"];
         exports.sentries.UPGRADES_TIER_0 = ["sentrySwarm", "sentryGun", "sentryTrap", "shinySentrySwarm", "shinySentryGun", "shinySentryTrap"];
     exports.retrograde.UPGRADES_TIER_0 = ["diepTanks", "digDig", "celestialBosses", "eliteBosses", "strangeBosses", "nostalgiaMenu", "scrappedMenu", "miscRetrograde"];
-        exports.diepTanks.UPGRADES_TIER_0 = ["diep2Tanks", "diepTank"];
-            exports.diep2Tanks.UPGRADES_TIER_0 = ["blaster", "gatlingGun", "machineFlank", "retroRifle", "buttbuttin", "blower", "quadTwin", "tornado", "subverter", "battery", "deathStar", "bonker", "protector", "doubleTrapGuard"];
-                exports.blaster.UPGRADES_TIER_3 = ["triBlaster", "splasher"];
-                exports.gatlingGun.UPGRADES_TIER_3 = ["retroSprayer", "accurator", "halfNHalf"];
-                exports.machineFlank.UPGRADES_TIER_3 = ["machineTriple", "halfNHalf"];
-                exports.retroRifle.UPGRADES_TIER_3 = ["sniperRifle", "rifleGuard", "spreadRifle"];
+        exports.diepTanks.UPGRADES_TIER_0 = ["diepTank"];
         exports.celestialBosses.UPGRADES_TIER_0 = ["paladin", "freyja", "zaphkiel", "nyx", "theia", "alviss", "tyr"];
         exports.eliteBosses.UPGRADES_TIER_0 = ["eliteDestroyer", "eliteGunner", "eliteSprayer", "eliteBattleship", "eliteSpawner"];
         exports.strangeBosses.UPGRADES_TIER_0 = ["roguePalisade", "rogueArmada", "nestKeeper", "eliteSkimmer", "summoner"];
         exports.nostalgiaMenu.UPGRADES_TIER_0 = ["oldSpreadshot", "bentBoomer", "quadBuilder", "quintuplet", "vulcan", "sniper3", "weirdSpike", "master", "oldCommander", "blunderbuss", "oldRimfire", "ransacker"];
-        exports.scrappedMenu.UPGRADES_TIER_0 = ["scrappedMenu2", "rocketeer", "crowbar", "peashooter", "autoTrapper", "megaTrapper", "railgun", "megaSpawner", "badDreadnought", "mender"];
-            exports.scrappedMenu2.UPGRADES_TIER_0 = ["scrappedMenu", "overcheese", "prodigy", "spawnerdrive", "rimfire", "productionist", "taser"];
-                exports.productionist.UPGRADES_TIER_0 = ["bismarck"];
+        exports.scrappedMenu.UPGRADES_TIER_0 = ["rocketeer", "crowbar", "peashooter", "autoTrapper", "megaTrapper", "railgun", "megaSpawner", "badDreadnought", "mender", "overcheese", "prodigy", "spawnerdrive", "rimfire", "productionist"];
         exports.miscRetrograde.UPGRADES_TIER_0 = ["tracker3", "tetraGunner", "worstTank"];
 
 // MISCELLANEOUS
