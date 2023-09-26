953e4c5f65087754ff9bca27ccce44b157f6e3ff
Frostbyte178 <frostbyte178@gmail.com>
2023-09-25 21:58:23 -0700

    dreadv2 foundations done
    except hexdreads screw those

--- server/modules/definitions/addons/dreadv2.js
+++ server/modules/definitions/addons/dreadv2.js
@@ -66,7 +66,6 @@ module.exports = ({ Class }) => {
 	    SHAPE: 0,
 	    COLOR: 6,
 	    SIZE: 12.5,
-	    EXTRA_SKILL: 18,
 	    SKILL_CAP: Array(10).fill(smshskl),
 	}
 	Class.genericSquarenought = {
@@ -75,7 +74,6 @@ module.exports = ({ Class }) => {
 	    SHAPE: 4,
 	    COLOR: 13,
 	    SIZE: 15,
-	    EXTRA_SKILL: 18,
 	    SKILL_CAP: Array(10).fill(smshskl),
 	}
 	Class.genericTrinought = {
@@ -84,7 +82,6 @@ module.exports = ({ Class }) => {
 	    SHAPE: 3.5,
 	    COLOR: 2,
 	    SIZE: 20,
-	    EXTRA_SKILL: 18,
 	    SKILL_CAP: Array(10).fill(smshskl),
 	}
 	Class.genericPentanought = {
@@ -93,7 +90,6 @@ module.exports = ({ Class }) => {
 	    SHAPE: 5.5,
 	    COLOR: 14,
 	    SIZE: 25,
-	    EXTRA_SKILL: 18,
 	    SKILL_CAP: Array(10).fill(smshskl),
 	}
 	Class.genericHexnought = {
@@ -102,7 +98,6 @@ module.exports = ({ Class }) => {
 	    SHAPE: 6,
 	    COLOR: 0,
 	    SIZE: 30,
-	    EXTRA_SKILL: 18,
 	    SKILL_CAP: Array(10).fill(smshskl),
 	}
 
@@ -111,6 +106,7 @@ module.exports = ({ Class }) => {
 		PARENT: ["genericEggnought"],
 	    LABEL: "Dreadnought",
 		LEVEL: 90,
+		EXTRA_SKILL: 18,
 		REROOT_UPGRADE_TREE: true,
 	}
 
@@ -350,6 +346,11 @@ module.exports = ({ Class }) => {
 	    LABEL: "Planet",
 	    TURRETS: [],
 	}
+	Class.moon_APSofficialdreadv2 = {
+	    PARENT: ["genericTrinought"],
+	    LABEL: "Moon",
+	    TURRETS: [],
+	}
 	Class.titan_APSofficialdreadv2 = {
 	    PARENT: ["genericTrinought"],
 	    LABEL: "Titan",
@@ -474,6 +475,11 @@ module.exports = ({ Class }) => {
 	    LABEL: "Astronomic",
 	    TURRETS: [],
 	}
+	Class.grandiose_APSofficialdreadv2 = {
+	    PARENT: ["genericPentanought"],
+	    LABEL: "Grandiose",
+	    TURRETS: [],
+	}
 	Class.leviathan_APSofficialdreadv2 = {
 	    PARENT: ["genericPentanought"],
 	    LABEL: "Leviathan",
@@ -484,9 +490,9 @@ module.exports = ({ Class }) => {
 	    LABEL: "Valrayvn",
 	    TURRETS: [],
 	}
-	Class.grandiose_APSofficialdreadv2 = {
+	Class.pegasus_APSofficialdreadv2 = {
 	    PARENT: ["genericPentanought"],
-	    LABEL: "Grandiose",
+	    LABEL: "Pegasus",
 	    TURRETS: [],
 	}
 
@@ -497,5 +503,133 @@ module.exports = ({ Class }) => {
 	Class.developer.UPGRADES_TIER_0.push("dread_APSofficialdreadv2");
 		Class.dread_APSofficialdreadv2.UPGRADES_TIER_1 = ["sword_APSofficialdreadv2", "pacifier_APSofficialdreadv2", "peacekeeper_APSofficialdreadv2", "invader_APSofficialdreadv2", "centaur_APSofficialdreadv2"];
 
-	// Build up to pentanoughts
+			Class.sword_APSofficialdreadv2.UPGRADES_TIER_M1 = ["sabre_APSofficialdreadv2", "gladius_APSofficialdreadv2"];
+				Class.sabre_APSofficialdreadv2.UPGRADES_TIER_M1 = ["bayonet_APSofficialdreadv2"];
+					Class.bayonet_APSofficialdreadv2.UPGRADES_TIER_M1 = ["javelin_APSofficialdreadv2"];
+				Class.gladius_APSofficialdreadv2.UPGRADES_TIER_M1 = ["blade_APSofficialdreadv2"];
+					Class.blade_APSofficialdreadv2.UPGRADES_TIER_M1 = ["rapier_APSofficialdreadv2"];
+
+			Class.pacifier_APSofficialdreadv2.UPGRADES_TIER_M1 = ["mediator_APSofficialdreadv2", "negotiator_APSofficialdreadv2"];
+				Class.mediator_APSofficialdreadv2.UPGRADES_TIER_M1 = ["mitigator_APSofficialdreadv2"];
+					Class.mitigator_APSofficialdreadv2.UPGRADES_TIER_M1 = ["diplomat_APSofficialdreadv2"];
+				Class.negotiator_APSofficialdreadv2.UPGRADES_TIER_M1 = ["appeaser_APSofficialdreadv2"];
+					Class.appeaser_APSofficialdreadv2.UPGRADES_TIER_M1 = ["arbitrator_APSofficialdreadv2"];
+
+			Class.peacekeeper_APSofficialdreadv2.UPGRADES_TIER_M1 = ["enforcer_APSofficialdreadv2", "executor_APSofficialdreadv2"];
+				Class.enforcer_APSofficialdreadv2.UPGRADES_TIER_M1 = ["suppressor_APSofficialdreadv2"];
+					Class.suppressor_APSofficialdreadv2.UPGRADES_TIER_M1 = ["retardant_APSofficialdreadv2"];
+				Class.executor_APSofficialdreadv2.UPGRADES_TIER_M1 = ["inhibitor_APSofficialdreadv2"];
+					Class.inhibitor_APSofficialdreadv2.UPGRADES_TIER_M1 = ["tyrant_APSofficialdreadv2"];
+
+			Class.invader_APSofficialdreadv2.UPGRADES_TIER_M1 = ["inquisitor_APSofficialdreadv2", "assailant_APSofficialdreadv2"];
+				Class.inquisitor_APSofficialdreadv2.UPGRADES_TIER_M1 = ["infiltrator_APSofficialdreadv2"];
+					Class.infiltrator_APSofficialdreadv2.UPGRADES_TIER_M1 = ["raider_APSofficialdreadv2"];
+				Class.assailant_APSofficialdreadv2.UPGRADES_TIER_M1 = ["aggressor_APSofficialdreadv2"];
+					Class.aggressor_APSofficialdreadv2.UPGRADES_TIER_M1 = ["gladiator_APSofficialdreadv2"];
+
+			Class.centaur_APSofficialdreadv2.UPGRADES_TIER_M1 = ["daemon_APSofficialdreadv2", "minotaur_APSofficialdreadv2"];
+				Class.daemon_APSofficialdreadv2.UPGRADES_TIER_M1 = ["hydra_APSofficialdreadv2"];
+					Class.hydra_APSofficialdreadv2.UPGRADES_TIER_M1 = ["cerberus_APSofficialdreadv2"];
+				Class.minotaur_APSofficialdreadv2.UPGRADES_TIER_M1 = ["charon_APSofficialdreadv2"];
+					Class.charon_APSofficialdreadv2.UPGRADES_TIER_M1 = ["cerberus_APSofficialdreadv2"];
+
+			Class.byte_APSofficialdreadv2.UPGRADES_TIER_M1 = ["automation_APSofficialdreadv2", "kilobyte_APSofficialdreadv2"];
+
+				Class.automation_APSofficialdreadv2.UPGRADES_TIER_M1 = ["mechanism_APSofficialdreadv2", "fusion_APSofficialdreadv2", "binary_APSofficialdreadv2", "exosphere_APSofficialdreadv2"];
+					Class.mechanism_APSofficialdreadv2.UPGRADES_TIER_M1 = ["skynet_APSofficialdreadv2"];
+					Class.fusion_APSofficialdreadv2.UPGRADES_TIER_M1 = ["supernova_APSofficialdreadv2"];
+					Class.binary_APSofficialdreadv2.UPGRADES_TIER_M1 = ["cipher_APSofficialdreadv2"];
+					Class.exosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["interstellar_APSofficialdreadv2"];
+
+				Class.kilobyte_APSofficialdreadv2.UPGRADES_TIER_M1 = ["megabyte_APSofficialdreadv2", "binary_APSofficialdreadv2", "trojan_APSofficialdreadv2", "hardware_APSofficialdreadv2"];
+					Class.megabyte_APSofficialdreadv2.UPGRADES_TIER_M1 = ["gigabyte_APSofficialdreadv2"];
+					// Class.binary_APSofficialdreadv2.UPGRADES_TIER_M1 = ["cipher_APSofficialdreadv2"];
+					Class.trojan_APSofficialdreadv2.UPGRADES_TIER_M1 = ["malware_APSofficialdreadv2"];
+					Class.hardware_APSofficialdreadv2.UPGRADES_TIER_M1 = ["software_APSofficialdreadv2"];
+
+			Class.atmosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["corona_APSofficialdreadv2", "thermosphere_APSofficialdreadv2"];
+
+				Class.corona_APSofficialdreadv2.UPGRADES_TIER_M1 = ["chromosphere_APSofficialdreadv2", "fusion_APSofficialdreadv2", "trojan_APSofficialdreadv2", "planet_APSofficialdreadv2"];
+					Class.chromosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["photosphere_APSofficialdreadv2"];
+					// Class.fusion_APSofficialdreadv2.UPGRADES_TIER_M1 = ["supernova_APSofficialdreadv2"];
+					// Class.trojan_APSofficialdreadv2.UPGRADES_TIER_M1 = ["malware_APSofficialdreadv2"];
+					Class.planet_APSofficialdreadv2.UPGRADES_TIER_M1 = ["astronomic_APSofficialdreadv2"];
+
+				Class.thermosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["mesosphere_APSofficialdreadv2", "exosphere_APSofficialdreadv2", "hardware_APSofficialdreadv2", "moon_APSofficialdreadv2"];
+					Class.mesosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["stratosphere_APSofficialdreadv2"];
+					// Class.exosphere_APSofficialdreadv2.UPGRADES_TIER_M1 = ["interstellar_APSofficialdreadv2"];
+					// Class.hardware_APSofficialdreadv2.UPGRADES_TIER_M1 = ["software_APSofficialdreadv2"];
+					Class.moon_APSofficialdreadv2.UPGRADES_TIER_M1 = ["grandiose_APSofficialdreadv2"];
+
+			Class.juggernaut_APSofficialdreadv2.UPGRADES_TIER_M1 = ["jumbo_APSofficialdreadv2", "colossal_APSofficialdreadv2"];
+
+				Class.jumbo_APSofficialdreadv2.UPGRADES_TIER_M1 = ["goliath_APSofficialdreadv2", "planet_APSofficialdreadv2", "moon_APSofficialdreadv2"];
+					Class.goliath_APSofficialdreadv2.UPGRADES_TIER_M1 = ["behemoth_APSofficialdreadv2"];
+					// Class.planet_APSofficialdreadv2.UPGRADES_TIER_M1 = ["astronomic_APSofficialdreadv2"];
+					// Class.moon_APSofficialdreadv2.UPGRADES_TIER_M1 = ["grandiose_APSofficialdreadv2"];
+
+				Class.colossal_APSofficialdreadv2.UPGRADES_TIER_M1 = ["titan_APSofficialdreadv2", "siren_APSofficialdreadv2", "harpy_APSofficialdreadv2"];
+					Class.titan_APSofficialdreadv2.UPGRADES_TIER_M1 = ["leviathan_APSofficialdreadv2"];
+					Class.siren_APSofficialdreadv2.UPGRADES_TIER_M1 = ["valrayvn_APSofficialdreadv2"];
+					Class.harpy_APSofficialdreadv2.UPGRADES_TIER_M1 = ["pegasus_APSofficialdreadv2"];
+
+	// Merge function
+	let mergedDreads = [];
+
+	function mergeDreadv2(weapon, body) {
+		let className = weapon.split("_")[0] + body;
+
+		weapon = ensureIsClass(Class, weapon);
+		body = ensureIsClass(Class, body);
+
+		let PARENT = ensureIsClass(Class, weapon.PARENT[0]),
+			BODY = JSON.parse(JSON.stringify(PARENT.BODY)),
+			GUNS = [],
+			TURRETS = [],
+			LABEL = weapon.LABEL + "-" + body.LABEL,
+			UPGRADES_TIER_0 = [];
+		
+		// Guns
+		if (weapon.GUNS) GUNS.push(...weapon.GUNS);
+		if (body.GUNS) GUNS.push(...body.GUNS);
+		
+		// Turrets
+		if (weapon.TURRETS) TURRETS.push(...weapon.TURRETS);
+		if (body.TURRETS) TURRETS.push(...body.TURRETS);
+		
+		// Body stat modification
+		if (weapon.BODY) for (let m in weapon.BODY) BODY[m] *= weapon.BODY[m];
+		if (body.BODY) for (let m in body.BODY) BODY[m] *= body.BODY[m];
+
+		// Upgrades
+		for (let w in weapon.UPGRADES_TIER_M1) {
+			for (let b in body.UPGRADES_TIER_M1) {
+				let weaponName = weapon.UPGRADES_TIER_M1[w],
+					bodyName = body.UPGRADES_TIER_M1[b];
+
+				if (!mergedDreads.includes(weaponName + bodyName))
+					mergeDreadv2(weaponName, bodyName);
+				
+				UPGRADES_TIER_0.push(weaponName.split("_")[0] + bodyName);
+			}
+		}
+
+		// Can he build it? Yes he can!
+		Class[className] = {
+			PARENT, BODY, LABEL, GUNS, TURRETS, UPGRADES_TIER_0,
+		};
+	}
+
+	// Initiate build for all dread paths and do upgrades for all eggnoughts
+	const eggnoughtWeapons = Class.dread_APSofficialdreadv2.UPGRADES_TIER_1;
+	const eggnoughtBodies = ["byte_APSofficialdreadv2", "atmosphere_APSofficialdreadv2", "juggernaut_APSofficialdreadv2"];
+	for (let w in eggnoughtWeapons) {
+		let weaponName = eggnoughtWeapons[w];
+		Class[weaponName].UPGRADES_TIER_1 = [];
+		for (let b in eggnoughtBodies) {
+			let bodyName = eggnoughtBodies[b];
+			mergeDreadv2(weaponName, bodyName);
+			Class[weaponName].UPGRADES_TIER_1.push(weaponName.split("_")[0] + bodyName);
+		}
+	}
 };
