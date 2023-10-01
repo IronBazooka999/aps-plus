68d7cdf85d432592e3a9c9760ba38b771355ceeb
Taureon <45183108+Taureon@users.noreply.github.com>
2023-10-02 00:32:22 +0200

    better fix

--- server/modules/gamemodes/bossRush.js
+++ server/modules/gamemodes/bossRush.js
@@ -143,8 +143,6 @@ class BossRush {
     }
 
     spawnWave(waveId) {
-        //hotfix because trio didnt test this????
-        Class.basic.UPGRADES_TIER_1.push('healer');
         //yell at everyone
         sockets.broadcast(`Wave ${waveId + 1} has arrived!`);
 
@@ -179,6 +177,8 @@ class BossRush {
     init() {
         for (let loc of room.bas1) {
             this.spawnDominator(loc, -1);
+        //hotfix because trio didnt test this????
+        Class.basic.UPGRADES_TIER_1.push('healer');
         }
         console.log('Boss rush initialized.');
     }
